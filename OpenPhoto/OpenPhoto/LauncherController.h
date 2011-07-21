84b536b323a4d821425f3e85a04e46cbf9c0e659
Patrick Santana <patricksan@me.com>
2011-07-21 11:57:35 +0200

    uploading picture

--- OpenPhoto/OpenPhoto/LauncherController.h
+++ OpenPhoto/OpenPhoto/LauncherController.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <Three20/Three20.h>
 
-@interface LauncherController : TTViewController <TTLauncherViewDelegate> {
+@interface LauncherController : TTViewController <TTLauncherViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
     TTLauncherView* launcherView;
 }
 @end
--- OpenPhoto/OpenPhoto/LauncherController.m
+++ OpenPhoto/OpenPhoto/LauncherController.m
@@ -36,5 +36,63 @@
 
 - (void) openPickerFromButton{
     NSLog(@"Get pictures");
+    UIImagePickerController *pickerController = [[UIImagePickerController
+                                                  alloc]
+                                                 init];
+    pickerController.sourceType =
+    UIImagePickerControllerSourceTypePhotoLibrary;
+    pickerController.delegate = self;
+    [self presentModalViewController:pickerController animated:YES];
+    [pickerController release];
 }
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *pickedImage = [info
+                            objectForKey:UIImagePickerControllerOriginalImage];
+   
+    // send message to the site. it is pickedImage
+    NSData *imageData = UIImageJPEGRepresentation(pickedImage, 1);
+    
+    NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+    [request setURL:[NSURL URLWithString:@"http://current.openphoto.me/photo/upload.json"]];
+    [request setHTTPMethod:@"POST"];
+
+    NSString *stringBoundary = [NSString stringWithString:@"0xKhTmLbOuNdArY"];
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",stringBoundary];
+    [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+//    NSMutableData *body = [NSMutableData data];
+//    [body appendData:[[NSString stringWithString:@"Content-Disposition: form-data; title=\"Patrick Teste\"; filename=\"photo.jpg\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+//    [body appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+//    [body appendData:imageData];
+//    
+//    [request setHTTPBody:body];
+    
+    NSMutableData *postBody = [NSMutableData data];
+    [postBody appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [postBody appendData:[NSData dataWithData:UIImageJPEGRepresentation(pickedImage,1)]];
+    
+    [request setHTTPBody:postBody];
+
+    NSLog(@"%@", [request allHTTPHeaderFields]);
+     NSLog(@"%@",[request valueForHTTPHeaderField:contentType]);
+    
+
+//    NSString *imageB64   = [self escapeString:[imageDatabase64Encoding]];  
+//Custom implementations, no built in base64 or HTTP escaping for iPhone
+    
+    [self dismissModalViewControllerAnimated:YES];
+
+    
+    NSURLResponse *response;
+    NSError *error = nil;
+    NSData *XMLResponse = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];   
+	NSString *jsonString = [[NSString alloc] initWithData:XMLResponse encoding:NSUTF8StringEncoding];
+    NSLog(@"Result = %@",jsonString);   
+        
+
+}
+
 @end
