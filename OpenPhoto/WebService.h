a8a965599f40f064bbf1b41aaab8162223156d64
Patrick Santana <patricksan@me.com>
2011-08-04 10:07:44 +0200

    added initial WebService calls in a separated file

---
+++ OpenPhoto/WebService.h
@@ -0,0 +1,13 @@
+//
+//  WebService.h
+//  OpenPhoto
+//
+//  Created by Patrick Santana on 03/08/11.
+//  Copyright 2011 OpenPhoto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebService : NSObject
+
+@end
---
+++ OpenPhoto/WebServiceInvoker.m
@@ -0,0 +1,23 @@
+//
+//  WebServiceInvoker.m
+//  OpenPhoto
+//
+//  Created by Patrick Santana on 03/08/11.
+//  Copyright 2011 OpenPhoto. All rights reserved.
+//
+
+#import "WebServiceInvoker.h"
+
+@implementation WebServiceInvoker
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
