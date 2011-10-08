a0072a6be7a54b04d8def1759a1927f347a70748
Patrick Santana <patricksan@me.com>
2011-10-08 18:44:53 +0200

    Closes #23 #21 #22 - Changes in the project configuration files to remove warning and move frameworks

--- OpenPhoto/WebService.h
+++ OpenPhoto/WebService.h
@@ -14,6 +14,8 @@
 #import "Reachability.h"
 #import "QSStrings.h"
 #import "Base64Utilities.h"
+#import "extThree20JSON/extThree20JSON.h"
+#import "extThree20JSON/NSString+SBJSON.h"
 
 // for validation internet
 @class Reachability;
