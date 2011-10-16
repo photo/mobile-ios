3453136b01998bff5571636f6acd109cddacfc45
Patrick Santana <patricksan@me.com>
2011-10-16 16:45:22 +0200

    format

--- OpenPhoto/WebService.m
+++ OpenPhoto/WebService.m
@@ -308,8 +308,8 @@
 - (BOOL) validateNetwork{
     // check for the network and if our server is reachable
     if (self.internetActive == NO || self.hostActive == NO){
-          return NO;
-        }
+        return NO;
+    }
     
     return YES;
 }
