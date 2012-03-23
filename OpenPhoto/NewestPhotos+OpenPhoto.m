3791d6159319561c7b39fb8a701216a7370317d0
Patrick Santana <patricksan@me.com>
2012-03-23 20:02:38 +0100

    #100 Adding new model

---
+++ OpenPhoto/NewestPhotos+OpenPhoto.m
@@ -0,0 +1,13 @@
+//
+//  NewestPhotos+OpenPhoto.m
+//  OpenPhoto
+//
+//  Created by Patrick Santana on 22/03/12.
+//  Copyright (c) 2012 OpenPhoto. All rights reserved.
+//
+
+#import "NewestPhotos+OpenPhoto.h"
+
+@implementation NewestPhotos (OpenPhoto)
+
+@end
