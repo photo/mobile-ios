d78b3e6212c2c132eab440c30fbc42cbc4313e0f
Patrick Santana <patricksan@me.com>
2012-04-03 00:22:47 +0200

    updated TestFlight

--- Frameworks/TestFlight/TestFlight.h
+++ Frameworks/TestFlight/TestFlight.h
@@ -6,7 +6,7 @@
 //  Copyright 2011 TestFlight. All rights reserved.
 
 #import <Foundation/Foundation.h>
-#define TESTFLIGHT_SDK_VERSION @"1.0beta1"
+#define TESTFLIGHT_SDK_VERSION @"1.0"
 #undef TFLog
 
 #if __cplusplus
