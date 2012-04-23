f349f830687513feabbf4c6fb9247be36921a611
Patrick Santana <patricksan@me.com>
2012-04-23 15:01:47 +0200

    New alert to be used in the application

---
+++ OpenPhoto/OpenPhotoAlertView.h
@@ -0,0 +1,27 @@
+//
+//  OpenPhotoAlertView.h
+//  OPMEAlert
+//
+//  Created by Patrick Santana on 23/04/12.
+//  Copyright 2012 OpenPhoto
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//  http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface OpenPhotoAlertView : NSObject
+    
+// init with text and duration
+- (id) initWithMessage:(NSString *) text duration:(NSInteger) time;
+- (void)showAlert;
+@end
