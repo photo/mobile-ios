af5d6093aaf1f35121fbe64a1358ff99728e2602
Patrick Santana <patricksan@me.com>
2012-04-30 13:41:02 -0700

    #141: Added code to share more on newest photos. Missing art.

--- OpenPhoto/NewestPhotoCell.m
+++ OpenPhoto/NewestPhotoCell.m
@@ -22,6 +22,7 @@
 #import "NewestPhotoCell.h"
 
 @implementation NewestPhotoCell
+
 @synthesize photo=_photo;
 @synthesize date=_date;
 @synthesize tags=_tags;
@@ -29,7 +30,10 @@
 @synthesize private=_private;
 @synthesize geoPositionButton=_geoPositionButton;
 @synthesize label=_label;
+@synthesize shareButton = _shareButton;
 @synthesize geoPosition=_geoPosition;
+@synthesize imageUrl=_imageUrl;
+@synthesize newestPhotosTableViewController=_newestPhotosTableViewController;
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
@@ -43,8 +47,6 @@
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
-    
-    // Configure the view for the selected state
 }
 
 - (void)dealloc {
@@ -56,13 +58,38 @@
     [self.private release];
     [self.geoPositionButton release];
     [self.geoPosition release];
+    [self.shareButton release];
+    [self.newestPhotosTableViewController release];
     [super dealloc];
 }
+
 - (IBAction)openGeoPosition:(id)sender {
     if (self.geoPosition != nil){
         NSString *url = [NSString stringWithFormat: @"http://maps.google.com/maps?q=%@",
                          [self.geoPosition stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url]];
+    }
+}
+
+- (IBAction)sharePhoto:(id)sender {
+    if (self.imageUrl != nil && self.newestPhotosTableViewController != nil){
+              
+        // create the item to share
+        SHKItem *item = [SHKItem URL:[NSURL URLWithString:self.imageUrl] title:self.label.text];
+        
+        // Get the ShareKit action sheet
+        SHKActionSheet *actionSheet = [SHKActionSheet actionSheetForItem:item];
+        
+        // ShareKit detects top view controller (the one intended to present ShareKit UI) automatically,
+        // but sometimes it may not find one. To be safe, set it explicitly
+        [SHK setRootViewController:self.newestPhotosTableViewController];
+        
+        // Display the action sheet
+        [actionSheet showFromTabBar:self.newestPhotosTableViewController.tabBarController.tabBar];
+        
+#ifdef TEST_FLIGHT_ENABLED
+        [TestFlight passCheckpoint:@"Newest Photos - Share More"];
+#endif  
     }
 }
 @end
