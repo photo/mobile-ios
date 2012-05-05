228464418469484015bd1406f108245dbbaa60f2
Patrick Santana <patricksan@me.com>
2012-05-05 14:36:51 -0700

    Added initial files for the sync and fixed the tag icon

---
+++ OpenPhoto/SyncViewController.m
@@ -0,0 +1,43 @@
+//
+//  SyncViewController.m
+//  OpenPhoto
+//
+//  Created by Patrick Santana on 05/05/12.
+//  Copyright (c) 2012 OpenPhoto. All rights reserved.
+//
+
+#import "SyncViewController.h"
+
+@interface SyncViewController ()
+
+@end
+
+@implementation SyncViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
