9f1ed994f602ddf145aa26ffab4d4f619efe6335
Patrick Santana <patricksan@me.com>
2012-05-21 14:12:57 +0200

    #91: added core data for the Sync functionality

---
+++ OpenPhoto/SyncPhotos.m
@@ -0,0 +1,18 @@
+//
+//  SyncPhotos.m
+//  OpenPhoto
+//
+//  Created by Patrick Santana on 21/05/12.
+//  Copyright (c) 2012 OpenPhoto. All rights reserved.
+//
+
+#import "SyncPhotos.h"
+
+
+@implementation SyncPhotos
+
+@dynamic filePath;
+@dynamic status;
+@dynamic fileHash;
+
+@end
