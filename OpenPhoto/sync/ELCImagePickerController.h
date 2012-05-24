b286fdb7d693d5189f7f6b897e271a7d6bf6f926
Patrick Santana <patricksan@me.com>
2012-05-24 10:10:14 +0200

    #91: added classes form ELCImagePicker. We will have in the OpenPhoto tree because a lot of changes will be needed.

---
+++ OpenPhoto/sync/ELCImagePickerController.h
@@ -0,0 +1,29 @@
+//
+//  ELCImagePickerController.h
+//  ELCImagePickerDemo
+//
+//  Created by Collin Ruffenach on 9/9/10.
+//  Copyright 2010 ELC Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELCImagePickerController : UINavigationController {
+
+	id delegate;
+}
+
+@property (nonatomic, assign) id delegate;
+
+-(void)selectedAssets:(NSArray*)_assets;
+-(void)cancelImagePicker;
+
+@end
+
+@protocol ELCImagePickerControllerDelegate
+
+- (void)elcImagePickerController:(ELCImagePickerController *)picker didFinishPickingMediaWithInfo:(NSArray *)info;
+- (void)elcImagePickerControllerDidCancel:(ELCImagePickerController *)picker;
+
+@end
+
