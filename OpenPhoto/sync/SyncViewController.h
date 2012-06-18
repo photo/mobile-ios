d97380ebaa17dedaa5529e81d1006ec84347542e
Patrick Santana <patricksan@me.com>
2012-06-18 15:51:55 +0200

    #91: Adding options

---
+++ OpenPhoto/sync/SyncViewController.h
@@ -0,0 +1,46 @@
+//
+//  SyncViewController.h
+//  OpenPhoto
+//
+//  Created by Patrick Santana on 18/06/12.
+//  Copyright (c) 2012 OpenPhoto. All rights reserved.
+//
+
+#import <AssetsLibrary/AssetsLibrary.h>
+#import "AssetsLibraryUtilities.h"
+#import "MBProgressHUD.h"
+#import "SyncPhotos+OpenPhoto.h"
+#import "ELCAsset.h"
+#import "ELCAlbumPickerController.h"
+#import "ELCAssetTablePicker.h"    
+#import "ELCAssetCell.h"
+
+@interface SyncViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+{
+	ALAssetsGroup *assetGroup;
+	
+	NSMutableArray *elcAssets;
+	int selectedAssets;
+	
+	id parent;
+	
+	NSOperationQueue *queue;
+    ALAssetsLibrary *library;
+    
+    NSMutableArray *imagesAlreadyUploaded;
+    
+    BOOL loaded;
+}
+
+@property (nonatomic, assign) id parent;
+@property (nonatomic, retain) ALAssetsGroup *assetGroup;
+@property (nonatomic, retain) NSMutableArray *elcAssets;
+@property (nonatomic, retain) NSMutableArray *imagesAlreadyUploaded;
+@property (retain, nonatomic) IBOutlet UITableView *tableView;
+
+-(int)totalSelectedAssets;
+-(void)preparePhotos;
+
+-(void)doneAction:(id)sender;
+
+@end
