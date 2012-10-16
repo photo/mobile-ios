3b4c11712e7b41e7a02f186a220a16ec70686986
Patrick Santana <patricksan@me.com>
2012-10-16 09:53:52 +0200

    added private constant

---
+++ Photo/PrivateConstants.h
@@ -0,0 +1,15 @@
+//
+//  PrivateConstants.h
+//  Photo
+//
+//  Created by Patrick Santana on 16/10/12.
+//  Copyright (c) 2012 Photo Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrivateConstants : NSObject
+
+extern NSString * const kPrivateFacebookAppId;
+extern NSString * const kPrivateTestFlightId;
+@end
---
+++ Photo/PrivateConstants.m
@@ -0,0 +1,16 @@
+//
+//  PrivateConstants.m
+//  Photo
+//
+//  Created by Patrick Santana on 16/10/12.
+//  Copyright (c) 2012 Photo Project. All rights reserved.
+//
+
+#import "PrivateConstants.h"
+
+@implementation PrivateConstants
+
+NSString * const kPrivateFacebookAppId=@"";
+NSString * const kPrivateTestFlightId=@"";
+
+@end
