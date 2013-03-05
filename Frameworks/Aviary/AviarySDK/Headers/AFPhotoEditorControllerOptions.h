dc1925651e7daddadadb2da26159303220d2f051
Patrick Santana <patricksan@me.com>
2013-03-05 11:27:15 +0100

    sharekit

---
+++ Frameworks/Aviary/AviarySDK/Headers/AFPhotoEditorControllerOptions.h
@@ -0,0 +1,11 @@
+//
+//  AFPhotoEditorControllerOptions.h
+//  AviarySDK
+//
+//  Copyright (c) 2012 Aviary, Inc. All rights reserved.
+//
+
+/**
+ Replaced by AFPhotoEditorCustomization.h in 2.5.0.
+ */
+#import "AFPhotoEditorCustomization.h"
